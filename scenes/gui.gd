df961751403397cd829ee2b05cbd8c351023b069
Luan Carlo <luanveloz@gmail.com>
2023-06-18 18:47:09 +0300

    Show FPS and first deploy config

--- scenes/gui.gd
+++ scenes/gui.gd
@@ -3,7 +3,12 @@ class_name GUI extends Control
 signal color_changing(color : Color)
 signal color_close()
 
+@onready var _fps := $FPS/Value as Label
 @onready var _change_color := $ChangeColor as Control
+
+
+func _process(_delta):
+	_fps.text = str(Engine.get_frames_per_second())
 
 
 func show_change_color(color : Color = Color.WHITE) -> void:
